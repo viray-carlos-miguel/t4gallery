4b16db1a90d2bcbe0a450dcac566e3d953b59090
viray-carlos-miguel <carlosviray2004@gmail.com>
2025-03-07 22:00:44 -0800

    nav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,12 +9,27 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
+function TopNav(){
+return (
+<nav className="flex w-full items-center justify-between p-4 text-x1 font-semibold border-b">
+  <div>Logo</div>
+  <div>Sign In</div>
+
+
+</nav>
+);
+
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
+      <body>
+      <TopNav/>
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,29 @@
 import Link from "next/link";
 
+const mockUrls = [
+"https://0ohpf8hz2w.ufs.sh/f/rngcbwM3WeZaCD8NoLgtpH7lFrRWjvxXs3obGcVdYgKzfqZN",
+"https://0ohpf8hz2w.ufs.sh/f/rngcbwM3WeZak7Jnx5NtMvR9sgITHlUf5EDq3VjwBAK0XLPa",
+"https://0ohpf8hz2w.ufs.sh/f/rngcbwM3WeZageVRuQKNt6O3SVRDz5GEdC8Bhse4PLrQK9w0",
+];
+
+const mockImages = mockUrls.map((url, index) =>({
+  id: index + 1, 
+  url,
+
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      hello (Gallery in progress)
-      Test 2
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {mockImages.map((image) => (
+          <div key = {image.id} className="w-48">
+            <img src={image.url} />
+
+          </div>
+        ))}
+        
+      </div>
     </main>
   );
 }
