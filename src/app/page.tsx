e0c090a6f5d419ee1dfc8b51dc4f4a8af226047c
viray-carlos-miguel <carlosviray2004@gmail.com>
2025-03-07 21:17:27 -0800

    test 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       hello (Gallery in progress)
+      Test 2
     </main>
   );
 }
