85374c1d3aa774fd4145b40b025ef2dfe965321f
viray-carlos-miguel <carlosviray2004@gmail.com>
2025-03-10 07:40:48 -0700

    Linking database to UI

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 - [x] Make it deploy
 - [x] scaffold with basic ui with mock data
 - [x] setup database using (postgresql)
+- [x] attach database to UI
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,34 +4,19 @@ import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-"https://0ohpf8hz2w.ufs.sh/f/rngcbwM3WeZaCD8NoLgtpH7lFrRWjvxXs3obGcVdYgKzfqZN",
-"https://0ohpf8hz2w.ufs.sh/f/rngcbwM3WeZak7Jnx5NtMvR9sgITHlUf5EDq3VjwBAK0XLPa",
-"https://0ohpf8hz2w.ufs.sh/f/rngcbwM3WeZageVRuQKNt6O3SVRDz5GEdC8Bhse4PLrQK9w0",
-];
-
-const mockImages = mockUrls.map((url, index) =>({
-  id: index + 1, 
-  url,
-
-}));
-
 export default async function HomePage() {
 
-const posts = await db.query.posts.findMany();
+const images = await db.query.images.findMany({
+  orderBy: (model, {desc}) => desc(model.id),
+});
 
   return (
     <main className="">
       <div className="flex flex-wrap justify-items-center gap-4">
-
-        {posts.map((posts)=> (
-          <div key={posts.id}>{posts.name}</div>
-
-        ))}
-        {mockImages.map((image) => (
+        {images.map((image) => (
           <div key = {image.id} className="w-48">
             <img src={image.url} />
-
+          {<div>{image.name}</div>}
           </div>
         ))}
         
