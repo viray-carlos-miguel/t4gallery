7b7c9307862a231664848f9c04818878b7aee3e6
viray-carlos-miguel <carlosviray2004@gmail.com>
2025-03-12 19:45:12 -0700

    img

--- next.config.js
+++ next.config.js
@@ -2,10 +2,16 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
+import { hostname } from "os";
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
 const config = {
+  images : {
+remotePatterns: [
+{hostname: "utfs.io"}
+],
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
